4dcf6a908638ea8de57e5a6a2f612b220aa7342a
Anti <kaybart@hotmail.de>
2023-03-20 00:35:20 +0100

    fixed length
    Add length to OpenAI response so response is not cut off.
    Add Comments for future features

--- Chatbot.py
+++ Chatbot.py
@@ -12,7 +12,7 @@ print("Moin, \nsie schreiben nun mit dem Chatbot, er wird ihnen nun weiterhelfen
 username = input("What is your Username?\n")
 if username == "bye": 
     print("Well Bye then")
-
+#Username in Datenbank einfügen
 
 client = input("Whats you Clients name?\n")
 
@@ -22,14 +22,32 @@ while client != "bye" and client == "" or not bool(re.search("[cC]\d\d\d\d", cli
 if client == "bye":
     print("Well Bye then")
 
+#Client in Datenbank einfügen
+
 user_question = input("What seems to be your Problem?\n")
 
 while user_question != "" and user_question != "bye" and user_question != "fixed":
-    #Fragen Loop für Kunden
+    
+    #Fragen in Datenbank mit einfügen
+    
     response = openai.Completion.create(
-            model="gpt-3.5-turbo",
+            model="text-davinci-003",
             prompt=user_question,
-            temperature=0.6,)
+            max_tokens=256,
+            temperature=0.6)
+    
     answer = response['choices'][0]['text']
+    #Antwort in Datenbank mit einfügen
+    
     print(answer)
-    user_question = input("If your question was answered and your problem was fixed please write 'fixed'\nIf your question was not fixed please try asking in a different way.\n")
+    user_question = input("\nIf your question was answered and your problem was fixed please write 'fixed'\nIf your problem was not fixed please try asking in a different way.\n")
+
+if user_question == "bye":
+    # Program exit in Datenbank mit einfügen und "Uncertainty" als exit grund hinzufügen
+    print("Well by then")
+    #program exit
+    
+elif user_question == "fixed":
+    #Datenbank input mit "Anfrage gelöst" oder änlichem hinzufügen
+    print("Thanks for your cooperation")
+    #program exit
