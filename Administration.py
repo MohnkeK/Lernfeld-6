8b72329cd0c1d12e52c20cf7fd959b1c159255c3
Anti <kaybart@hotmail.de>
2023-03-20 02:34:22 +0100

    Add Administration Tool

---
+++ Administration.py
@@ -0,0 +1,8 @@
+# Datenbankzugriff
+ 
+#Suche nach Probelemen die nicht gefixed wurden
+#Auswahl von Liste an Clients welche in die "clients" variable geschrieben wird
+
+#Ping test zum client 
+#rdp zum client
+#info zum client
