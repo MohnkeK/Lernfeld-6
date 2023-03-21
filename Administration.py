b868e96275ed578d535a2d162f523adde914718f
AntiAlles <kaybart@hotmail.de>
2023-03-21 12:45:03 +0100

    Add ticket check

--- Administration.py
+++ Administration.py
@@ -1,11 +1,13 @@
 from data_class import *
 
-
 db.connect()
 if Ticket.select().where(Ticket.is_problem_fixed == 0).exists():
     print("Problems occured. please fix men")
- 
-#Suche nach Probelemen die nicht gefixed wurden
+    t = Ticket.get(Ticket.is_problem_fixed == 0)
+    print("Ticket ID: " + "Client" + t.CI + "Problem: " + t.user_input + "Created: " + t.ticket_create_date)
+    
+
+
 #Auswahl von Liste an Clients welche in die "clients" variable geschrieben wird
 
 #Ping test zum client 
