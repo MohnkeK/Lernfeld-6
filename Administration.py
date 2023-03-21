e3a62394008849507d4181b91ab16217b6ec3225
Anti <kaybart@hotmail.de>
2023-03-21 08:19:46 +0100

    Create database connection

--- Administration.py
+++ Administration.py
@@ -1,4 +1,9 @@
-# Datenbankzugriff
+from data_class import *
+
+
+db.connect()
+if Ticket.select().where(Ticket.is_problem_fixed == 0).exists():
+    print("Problems occured. please fix men")
  
 #Suche nach Probelemen die nicht gefixed wurden
 #Auswahl von Liste an Clients welche in die "clients" variable geschrieben wird
