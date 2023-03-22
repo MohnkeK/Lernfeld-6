a41a819c656d04e44030641a5e57cd50e189dd22
Anti <kaybart@hotmail.de>
2023-03-22 17:58:09 +0100

    Add CI output and close argument

--- Administration.py
+++ Administration.py
@@ -2,23 +2,30 @@ from data_class import *
 
 db.connect()
 admin_input = ""
+ticket_list = []
 print("Customer contact requests:\n")
 
-while admin_input != "close": 
+while admin_input != "close":
     if Ticket.select().where(Ticket.is_problem_fixed == 0).exists():
         print("Problems occured. please fix men")
-        for id in Ticket.get(Ticket.is_problem_fixed == 0):
-            t = Ticket.get(Ticket.is_problem_fixed == 0)
-            print(t)
-            print("Ticket ID: " + str(t) + " | Client: " + t.CI + " | Problem: " + t.user_input + " | Created: " + str(t.ticket_create_date))
+        query = Ticket.select().where(Ticket.is_problem_fixed == 0)
+        for entry in query:
+            print("Ticket ID: " + str(entry) + " | Client: " + entry.CI + " | Problem: " +
+                  entry.user_input + " | Created: " + str(entry.ticket_create_date))
         admin_input = input("Please choose a ticket ID to work on:\n")
+        while admin_input != "close" and admin_input != "":
+            print(admin_input)
+            entry = Ticket.get(Ticket.id == admin_input)
+            client = entry.CI
+            print("Ticket ID: " + str(entry) + " | Client: " + entry.CI + " | Problem: " + entry.user_input +
+                  " | Answer: " + entry.final_chat_output + " | Created: " + str(entry.ticket_create_date) + "\n")
+            admin_input = input("Please choose your option: \n")
+            # ping test
+            # rdp
+            # general info
+
     else:
         print("No Tickets available.... Refreshing in 10 seconds")
         time.sleep(10)
-        
 
-#Auswahl von Liste an Clients welche in die "clients" variable geschrieben wird
-
-#Ping test zum client 
-#rdp zum client
-#info zum client
+print("Goodbye")
